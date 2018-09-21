966b5fd91202a4c722e8113c6eb93ed46a0c14ef
nlabrada <nlabrada12@gmail.com>
2018-09-20 22:59:16 -0500

    done with customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 });
 
 //// making the connection and calling functions
-connection.connect(function(err){
+connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id: " + connection.threadId);
   showAlmostEverything();
@@ -20,11 +20,11 @@ connection.connect(function(err){
 
 //// function for showing (almost) everything!
 function showAlmostEverything() {
-  connection.query("SELECT item_id, product_name, price FROM products", function(err, res){
+  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
     if (err) throw err;
     console.log("ITEM ID     PRODUCT                PRICE");
     console.log("----------------------------------------");
-    for (i=0; i<res.length; i++){
+    for (i=0; i<res.length; i++) {
     var space = "          ";
     var productsArray = [
       res[i].item_id + space,
@@ -45,7 +45,32 @@ inquirer.prompt([
   {
     name: "askQuantity",
     message: "🆗 how much we talkin'?"
-  }
-], function( answers ) {
-  // Use user feedback for... whatever!!
+  },
+]).then(function(checking) {
+  connection.query("SELECT * FROM products WHERE item_id=?", [checking.askId], function(err, res){
+    var invalid = /[a-z]/gi;
+    if (err) throw err;
+    if (res === invalid || res.length ===0) {
+      console.log("dang, enter a number nextime");
+      connection.end();
+    } else {
+      buyThatThing(res[0], checking.askQuantity);
+    };
+  });
 });
+
+//// buying the item and updating db!
+
+function buyThatThing(res, askQuantity) {
+  if (askQuantity > res.stock_quantity) {
+    console.log("sorry, my dude. not enough in-stock!");
+    connection.end();
+  } else if (askQuantity <= res.stock_quantity) {
+    connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [parseFloat(res.stock_quantity) - askQuantity, res.item_id], function(err){
+      if (err) throw err;
+    });
+    var total = parseInt(askQuantity) * parseFloat(res.price);
+    console.log("nice choice! it's all yours 😎 Your total comes out to $" + total);
+    connection.end();
+  };
+};
